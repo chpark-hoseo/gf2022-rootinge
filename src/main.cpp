8d145cb4572ce6d6bd14142712d4defc43d0e5d0
rootinge <jhpark5129@gmail.com>
2022-09-12 20:55:04 +0900

    HelloSDL-과제
    1. 윈도우 바탕화면 파랑색
    2. 윈도우 생성 다르게
    3. 프로그램 10초 후 종료

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* args[])
 		g_pWindow = SDL_CreateWindow("Game Framework",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED, 1024, 768,
-			SDL_WINDOW_SHOWN);
+			SDL_WINDOW_BORDERLESS|SDL_WINDOW_MAXIMIZED);
 
 		if (g_pWindow != 0) {
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
@@ -21,11 +21,11 @@ int main(int argc, char* args[])
 	}
 	
 	SDL_SetRenderDrawColor(
-		g_pRenderer, 0, 0, 0, 255);
+		g_pRenderer, 0, 0, 255, 255);
 	SDL_RenderClear(g_pRenderer);
 	SDL_RenderPresent(g_pRenderer);
 
-	SDL_Delay(5000);
+	SDL_Delay(10000);
 	SDL_Quit();
 
 	return 0;
