d4e120ce8f5afb8651d86ac0907bdf9a5f53268c
rootinge <jhpark5129@gmail.com>
2022-09-16 13:32:08 +0900

    HelloSDL분리하기
    HelloSDL분리하기
    1. main.cpp 잘못된 부분 수정
    2. 색 랜덤 변경

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,69 @@
 ﻿#include <SDL2/SDL.h>
+#include <stdlib.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false; // render함수의 반복하는지의 변수
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-		g_pWindow = SDL_CreateWindow("Game Framework",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 1024, 768,
-			SDL_WINDOW_BORDERLESS|SDL_WINDOW_MAXIMIZED);
+        g_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, height, width, flags);
 
-		if (g_pWindow != 0) {
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else {
-		return 1;
-	}
-	
-	SDL_SetRenderDrawColor(
-		g_pRenderer, 0, 0, 255, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
+        if (g_pWindow != 0) {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
 
-	SDL_Delay(10000);
-	SDL_Quit();
+    }
+    else {
+        return false;
+    }
 
-	return 0;
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+    return true;
 }
+
+void render()
+{
+    SDL_RenderClear(g_pRenderer);
+
+    // 그리기 수행
+   
+
+    SDL_RenderPresent(g_pRenderer);
+}
+
+void Update()
+{
+    // 색 랜던 변경
+    SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
+    SDL_Delay(1000);
+}
+
+int main(int argc, char* argv[])
+{
+    if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 640, 480,
+        SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1; // something's wrong
+    }
+
+    while (g_bRunning)
+    {
+        // handle input - update - render
+        Update();
+        render();
+    }
+
+    SDL_Quit();
+    return 0;
+}
+
