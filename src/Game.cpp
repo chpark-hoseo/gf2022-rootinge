118fe18904ea41362f1d850566ddc640d39d646c
rootinge <jhpark5129@gmail.com>
2022-09-23 13:59:29 +0900

    SDL 기본 그리기
    전에 올렸던 실습 잘못올려 다시올립니다.
    
    rider그림 화면 출력

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,8 +9,25 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
             m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
             if (m_pRenderer != 0) {
-                SDL_SetRenderDrawColor(
-                    m_pRenderer, 255, 255, 255, 255);
+                /*SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);*/
+
+                SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+
+                m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+                SDL_FreeSurface(pTempSurface);
+                // 원본상자의 너비/높이 설정
+                SDL_QueryTexture(m_pTexture, NULL, NULL,
+                    &m_sourceRectangle.w, &m_sourceRectangle.h);
+                // 대상상자의 너비/높이 설정
+                m_destinationRectangle.w = m_sourceRectangle.w;
+                m_destinationRectangle.h = m_sourceRectangle.h;
+                //원본상자/대상상자의 위치 설정
+                m_destinationRectangle.x = m_sourceRectangle.x = 0;
+                m_destinationRectangle.y = m_sourceRectangle.y = 0;
+
+
             }
             else {
                 return false; // 랜더러 생성 실패
@@ -36,7 +53,9 @@ void Game::update()
 void Game::render()
 {
     SDL_RenderClear(m_pRenderer);
+    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
     SDL_RenderPresent(m_pRenderer);
+
 }
 
 bool Game::running()
@@ -66,3 +85,4 @@ void Game::clean()
     SDL_DestroyRenderer(m_pRenderer);
     SDL_Quit();
 }
+
