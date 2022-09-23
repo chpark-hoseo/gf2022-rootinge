4f0c5d05886aa2abce1bdebe858177810f4018f7
rootinge <jhpark5129@gmail.com>
2022-09-23 13:14:38 +0900

    Game 클래스화
    Game 클래스화 실습

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,20 @@
-﻿#include <SDL2/SDL.h>
-#include <stdlib.h>
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false; // render함수의 반복하는지의 변수
-int num = 0;
-
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
-void render();
-void Update();
-
+﻿#include "Game.h"
+Game* g_game = 0;
 
 int main(int argc, char* argv[])
 {
-    if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 640, 480,
-        SDL_WINDOW_SHOWN))
+    g_game = new Game();
+
+    g_game->init("Game Class", 100, 100, 640, 480, 0);
+
+    while (g_game->running())
     {
-        g_bRunning = true;
-    }
-    else
-    {
-        return 1;
+        g_game->handleEvents();
+        g_game->update();
+        g_game->render();
     }
 
-    while (g_bRunning)
-    {
-        // handle input - update - render
-        if (num == 10)
-        {
-            break;
-        }
-        else
-        {
-            num++;
-        }
-        Update();
-        render();
-    }
+    g_game->clean();
 
-    SDL_Quit();
     return 0;
 }
-
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-
-        g_pWindow = SDL_CreateWindow(
-            title, xpos, ypos, height, width, flags);
-
-        if (g_pWindow != 0) {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
-    }
-    else {
-        return false;
-    }
-
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-    return true;
-}
-
-void render()
-{
-    SDL_RenderClear(g_pRenderer);
-    // 그리기 수행
-    SDL_RenderPresent(g_pRenderer);
-}
-
-void Update()
-{
-    // 색 랜던 변경
-    SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
-    SDL_Delay(1000);
-}
-
-
-
