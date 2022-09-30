ddbb2345bcedb695cc44b22c7058430bf63c53cb
rootinge <jhpark5129@gmail.com>
2022-09-30 13:46:35 +0900

    원본상자/대상상자-과제
    1. 사진 화면이등
    2. 사진 보여지는 부분 변경

--- src/Game.cpp
+++ src/Game.cpp
@@ -36,23 +36,21 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     SDL_QueryTexture(m_pTexture, NULL, NULL,
         &m_sourceRectangle.w, &m_sourceRectangle.h);
     // 대상상자의 너비/높이 설정
-    /*m_destinationRectangle.w = m_sourceRectangle.w;
-    m_destinationRectangle.h = m_sourceRectangle.h;*/
-    
+
     //원본상자
     m_sourceRectangle.w = 50;
     m_sourceRectangle.h = 50;
     //대상상자
-    m_destinationRectangle.w = 50;
-    m_destinationRectangle.h = 50;
+    m_destinationRectangle.w = m_sourceRectangle.w;
+    m_destinationRectangle.h = m_sourceRectangle.h;
 
 
     //원본상자/대상상자의 위치 설정
-    m_sourceRectangle.x = 0;
-    m_sourceRectangle.y = 0;
+    m_sourceRectangle.x = 50;
+    m_sourceRectangle.y = 50;
 
-    m_destinationRectangle.x = 0;
-    m_destinationRectangle.y = 0;
+    m_destinationRectangle.x = 200;
+    m_destinationRectangle.y = 200;
 
     m_bRunning = true;
     m_turn = true;
