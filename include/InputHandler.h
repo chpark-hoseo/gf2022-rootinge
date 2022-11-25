e9841c5da4bc6f5a855761dee06bd396186f9f48
rootinge <jhpark5129@gmail.com>
2022-11-25 13:36:38 +0900

    마우스입력
    1. 실습 1
    2. 실습 2

--- include/InputHandler.h
+++ include/InputHandler.h
@@ -1,6 +1,14 @@
 #pragma once
-
 #include <SDL2/SDL.h> 
+#include <vector>
+#include "Vector2D.h"
+
+enum mouse_buttons {
+    LEFT = 0,
+    MIDDLE = 1,
+    RIGHT = 2
+};
+
 class InputHandler {
 public:
     ~InputHandler() {}
@@ -13,10 +21,29 @@ public:
     bool isKeyDown(SDL_Scancode key);
     void update();
     void clean() {}
+    bool getMouseButtonState(int buttonNumber);
+    Vector2D* getMousePosition();
+
+    void onMouseMove(SDL_Event event);
+    void onMouseButtonDown(SDL_Event event);
+    void onMouseButtonUp(SDL_Event event);
+    void onKeyDown();
+    void onKeyUp();
 
 private:
-    InputHandler() {}
+    
+
+    std::vector<bool> m_mouseButtonStates;
+    Vector2D* m_mousePosition;
+
     static InputHandler* s_pInstance;
     const Uint8* m_keystates;
+    InputHandler() {
+        m_mousePosition = new Vector2D(0, 0);
+        for (int i = 0; i < 3; i++) {
+            m_mouseButtonStates.push_back(false);
+        }
+    }
 };
+
 typedef InputHandler TheInputHandler;
--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,4 +1,5 @@
 #include "Enemy.h"
+#include "InputHandler.h"
 
 Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams) {}
 
@@ -9,9 +10,20 @@ void Enemy::draw()
 
 void Enemy::update()
 {
+    handleInput();
     m_currentFrame = ((SDL_GetTicks() / 100) % 6);
-    m_acceleration.setX(1);
     SDLGameObject::update();
+}
+
+
+void Enemy::handleInput()
+{
+    if (TheInputHandler::Instance()->getMouseButtonState(LEFT)) {
+        printf("shoot \n");
+    }
+    Vector2D* vec = TheInputHandler::Instance()->getMousePosition();
+    m_velocity = (*vec - m_position) / 100;
+
 }
 
 void Enemy::clean() {}
