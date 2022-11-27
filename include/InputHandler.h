333b4da47d1c516361b7e12314694c512d023525
rootinge <jhpark5129@gmail.com>
2022-11-27 19:53:16 +0900

    마우스 입력
    마우스 입력

--- include/InputHandler.h
+++ include/InputHandler.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <SDL2/SDL.h> 
+#include <vector>
+
+enum mouse_buttons {
+    LEFT = 0,
+    MIDDLE = 1,
+    RIGHT = 2
+};
 
 class InputHandler {
 public:
@@ -14,9 +21,21 @@ public:
     void update();
     void clean() {}
 
+    bool getMouseButtonState(int buttonNumber);
+    Vector2D* getMousePosition();
+
+    void onMouseMove(SDL_Event event);
+    void onMouseButtonDown(SDL_Event event);
+    void onMouseButtonUp(SDL_Event event);
+    void onKeyDown();
+    void onKeyUp();
+
 private:
-    InputHandler() {}
+    InputHandler();
     static InputHandler* s_pInstance;
     const Uint8* m_keystates;
+
+    std::vector<bool> m_mouseButtonStates;
+    Vector2D* m_mousePosition;
 };
 typedef InputHandler TheInputHandler;
